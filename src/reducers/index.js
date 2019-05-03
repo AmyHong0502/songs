278ac4719c19fa23153a150aab68d48e348c9ce5
Amy Hong <git@amyhong.dev>
2019-05-02 22:27:08 -0700

    feat: add reducers - songs and selectedSong

---
+++ src/reducers/index.js
@@ -0,0 +1,24 @@
+const songsReducer = () => {
+  return [
+    {
+      title: 'Lorem Ipsum',
+      duration: '4:03'
+    },
+    {
+      title: 'Foo Song',
+      duration: '5:12'
+    },
+    {
+      title: 'Bar Bar',
+      duration: '2:34'
+    }
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === 'SONG_SELECTED') {
+    return action.payload;
+  }
+
+  return selectedSong;
+};
