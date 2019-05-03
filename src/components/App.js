b84f14a76229155f78e039319ef838cfe468fb0b
Amy Hong <git@amyhong.dev>
2019-05-02 22:39:54 -0700

    feat: add SongList component

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import SongList from './SongList';
 import { selectSong } from '../actions';
 
 const App = () => {
-  return <div>Hi</div>;
+  return (
+    <div>
+      <SongList />
+    </div>
+  );
 };
 
 export default App;
